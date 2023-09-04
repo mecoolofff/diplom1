ab53743265c9b03cdba2a1477829db1300211405
mecoolofff <136811940+mecoolofff@users.noreply.github.com>
2023-09-05 01:10:52 +0300

    Update SQL.py

--- SQL.py
+++ SQL.py
@@ -1,11 +1,11 @@
-# 1 задание
+# Список логинов курьеров с количеством их заказов в статусе «В доставке» (поле inDelivery = true)
 # SELECT c.login, o.inDelivery, COUNT(*) AS total_orders
 # FROM "Couriers" c
 # JOIN "Orders" o ON c.id = o.id
 # WHERE o.inDelivery = true
 # GROUP BY c.login;
 
-# 2 задание
+# Все трекеры заказов и их статусы
 # SELECT track,
 # CASE
 # WHEN finished = true THEN 2
